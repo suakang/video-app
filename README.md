39f0b4373b89256d504e48bfcd4ad53b17dd3d0d
SuA Kang <sua@suakang.net>
2020-09-23 12:41:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Search & Watch YouTube Videos
+
+# Tools & Features
+- Pull YouTube API
+- Search YouTube video via keyword
+- Watch & Re-render
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({ title }) => {
+  return (
+    <div className="search-bar ui segment">
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+export default Header;
